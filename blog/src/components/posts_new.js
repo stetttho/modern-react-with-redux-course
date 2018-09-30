5040d51d45dd7333ea4f951c9d3929d550aedfe5
Thomas Stettler <stetttho@students.zhaw.ch>
2018-09-30 20:45:39 +0200

    up to lesson 98

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -21,7 +24,10 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -44,6 +50,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -70,4 +77,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null,{ createPost })(PostsNew)
+);
